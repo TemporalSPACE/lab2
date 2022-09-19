593b28ee98e93bd59542c52aa28c7d13d70d6b53
TemporalSPACE <reverteadriel@gmail.com>
2022-09-19 02:37:49 +0000

    Fixed attack

--- app/models/attack.rb
+++ app/models/attack.rb
@@ -3,7 +3,7 @@ class Attack < ApplicationRecord
 
     belongs_to :victim  
     
-    validate: :solo_asustar_mayores
+    validate :solo_asustar_mayores
 
     def solo_asustar_mayores
         if ((monster.scare_level > 5) && ((Date.today.year - victim.birthdate.year) <18))
